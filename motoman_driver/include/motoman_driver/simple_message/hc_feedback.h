323c6bf576e90d9f57fe283add5334711fe5d6a9
Ted Miller <ted.miller@motoman.com>
2025-05-27 14:45:56 -0400

    Remove unused `HcFeedback` class

--- motoman_driver/include/motoman_driver/simple_message/hc_feedback.h
+++ motoman_driver/include/motoman_driver/simple_message/hc_feedback.h
@@ -1,98 +1,98 @@
-
-#ifndef MOTOMAN_DRIVER_SIMPLE_MESSAGE_HC_FEEDBACK_H
-#define MOTOMAN_DRIVER_SIMPLE_MESSAGE_HC_FEEDBACK_H
-
-#ifndef FLATHEADERS
-#include "simple_message/joint_data.h"
-#include "simple_message/simple_message.h"
-#include "simple_message/simple_serialize.h"
-#include "simple_message/shared_types.h"
-#include "simple_message/joint_feedback.h"
-//#include "simple_message/messages/hc_feedback_message.h"
-#else
-#include "joint_data.h"        // NOLINT(build/include)
-#include "simple_message.h"    // NOLINT(build/include)
-#include "simple_serialize.h"  // NOLINT(build/include)
-#include "shared_types.h"      // NOLINT(build/include)
-#include "joint_feedback.h"    // NOLINT(build/include)
-//#include "messages/hc_feedback_message.h"
-#endif
-
-#include<vector>
-
-namespace industrial
-{
-    namespace hc_feedback
-    {
-
-        class HcFeedback : public industrial::simple_serialize::SimpleSerialize
-        {
-        public:
-            /**
-             * \brief Default constructor
-             *
-             * This method creates empty data.
-             *
-             */
-            HcFeedback(void);
-            /**
-             * \brief Destructor
-             *
-             */
-            ~HcFeedback(void);
-
-            /**
-             * \brief Initializes a empty hc feedback
-             *
-             */
-            void init();
-
-            /**
-             * \brief Copies the passed in value
-             *
-             * \param src (value to copy)
-             */
-            void copyFrom(HcFeedback& src);
-
-            /**
-             * \brief == operator implementation
-             *
-             * \return true if equal
-             */
-            bool operator==(HcFeedback& rhs);
-
-            industrial::shared_types::shared_int getGroupNumber();
-
-            std::vector<industrial::shared_types::shared_real> getJointTorques();
-
-            std::vector<industrial::shared_types::shared_real> getTcpForces();
-
-            /**
-             * \brief check the validity state for a given field
-             * @param field field to check
-             * @return true if specified field contains valid data
-             */
-
-             // Overrides - SimpleSerialize
-            bool load(industrial::byte_array::ByteArray* buffer);
-            bool unload(industrial::byte_array::ByteArray* buffer);
-            unsigned int byteLength()
-            {
-                return sizeof(industrial::shared_types::shared_int) + 12 * sizeof(industrial::shared_types::shared_real);
-            }
-
-        private:
-            /**
-             * \brief Number of groups attached to the controller
-             */
-            industrial::shared_types::shared_int group_number;
-
-            std::vector<industrial::shared_types::shared_real> jointTorques;
-
-            std::vector<industrial::shared_types::shared_real> tcpForces;
-        };
-    }  // namespace 
-}  // namespace industrial
-
-
-#endif  // MOTOMAN_DRIVER_SIMPLE_MESSAGE_HC_FEEDBACK_H
+//
+//#ifndef MOTOMAN_DRIVER_SIMPLE_MESSAGE_HC_FEEDBACK_H
+//#define MOTOMAN_DRIVER_SIMPLE_MESSAGE_HC_FEEDBACK_H
+//
+//#ifndef FLATHEADERS
+//#include "simple_message/joint_data.h"
+//#include "simple_message/simple_message.h"
+//#include "simple_message/simple_serialize.h"
+//#include "simple_message/shared_types.h"
+//#include "simple_message/joint_feedback.h"
+////#include "simple_message/messages/hc_feedback_message.h"
+//#else
+//#include "joint_data.h"        // NOLINT(build/include)
+//#include "simple_message.h"    // NOLINT(build/include)
+//#include "simple_serialize.h"  // NOLINT(build/include)
+//#include "shared_types.h"      // NOLINT(build/include)
+//#include "joint_feedback.h"    // NOLINT(build/include)
+////#include "messages/hc_feedback_message.h"
+//#endif
+//
+//#include<vector>
+//
+//namespace industrial
+//{
+//    namespace hc_feedback
+//    {
+//
+//        class HcFeedback : public industrial::simple_serialize::SimpleSerialize
+//        {
+//        public:
+//            /**
+//             * \brief Default constructor
+//             *
+//             * This method creates empty data.
+//             *
+//             */
+//            HcFeedback(void);
+//            /**
+//             * \brief Destructor
+//             *
+//             */
+//            ~HcFeedback(void);
+//
+//            /**
+//             * \brief Initializes a empty hc feedback
+//             *
+//             */
+//            void init();
+//
+//            /**
+//             * \brief Copies the passed in value
+//             *
+//             * \param src (value to copy)
+//             */
+//            void copyFrom(HcFeedback& src);
+//
+//            /**
+//             * \brief == operator implementation
+//             *
+//             * \return true if equal
+//             */
+//            bool operator==(HcFeedback& rhs);
+//
+//            industrial::shared_types::shared_int getGroupNumber();
+//
+//            std::vector<industrial::shared_types::shared_real> getJointTorques();
+//
+//            std::vector<industrial::shared_types::shared_real> getTcpForces();
+//
+//            /**
+//             * \brief check the validity state for a given field
+//             * @param field field to check
+//             * @return true if specified field contains valid data
+//             */
+//
+//             // Overrides - SimpleSerialize
+//            bool load(industrial::byte_array::ByteArray* buffer);
+//            bool unload(industrial::byte_array::ByteArray* buffer);
+//            unsigned int byteLength()
+//            {
+//                return sizeof(industrial::shared_types::shared_int) + 12 * sizeof(industrial::shared_types::shared_real);
+//            }
+//
+//        private:
+//            /**
+//             * \brief Number of groups attached to the controller
+//             */
+//            industrial::shared_types::shared_int group_number;
+//
+//            std::vector<industrial::shared_types::shared_real> jointTorques;
+//
+//            std::vector<industrial::shared_types::shared_real> tcpForces;
+//        };
+//    }  // namespace 
+//}  // namespace industrial
+//
+//
+//#endif  // MOTOMAN_DRIVER_SIMPLE_MESSAGE_HC_FEEDBACK_H
--- motoman_driver/src/simple_message/hc_feedback.cpp
+++ motoman_driver/src/simple_message/hc_feedback.cpp
@@ -1,104 +1,104 @@
-
-#include <vector>
-
-#ifndef FLATHEADERS
-#include "motoman_driver/simple_message/hc_feedback.h"
-#include "simple_message/shared_types.h"
-#include "simple_message/log_wrapper.h"
-#else
-#include "hc_feedback.h"  // NOLINT(build/include)
-#include "shared_types.h"       // NOLINT(build/include)
-#include "log_wrapper.h"        // NOLINT(build/include)
-#endif
-
-//using industrial::hc_feedback_message::HcFeedbackMessage;
-using industrial::joint_feedback::JointFeedback;
-
-namespace industrial
-{
-    namespace hc_feedback
-    {
-
-        HcFeedback::HcFeedback(void)
-        {
-            this->init();
-        }
-
-        HcFeedback::~HcFeedback(void)
-        {
-        }
-
-        void HcFeedback::init()
-        {
-        }
-
-        void HcFeedback::copyFrom(HcFeedback& src)
-        {
-            group_number = src.getGroupNumber();
-            jointTorques = src.getJointTorques();
-            tcpForces = src.getTcpForces();
-        }
-
-        bool HcFeedback::operator==(HcFeedback& rhs)
-        {
-            return (group_number == rhs.getGroupNumber() &&
-                jointTorques == rhs.getJointTorques() &&
-                tcpForces == rhs.getTcpForces());
-        }
-
-        bool HcFeedback::load(industrial::byte_array::ByteArray* buffer)
-        {
-            LOG_COMM("Executing hc feedback load");
-
-            if (!buffer->load(this->group_number))
-            {
-                LOG_ERROR("Failed to load hc feedback groups_number");
-                return false;
-            }
-
-            if (!buffer->load(this->jointTorques))
-            {
-                LOG_ERROR("Failed to load jointTorques");
-                return false;
-            }
-
-            if (!buffer->load(this->tcpForces))
-            {
-                LOG_ERROR("Failed to load jointTorques");
-                return false;
-            }
-
-            LOG_COMM("hc feedback successfully loaded");
-            return true;
-        }
-
-        bool HcFeedback::unload(industrial::byte_array::ByteArray* buffer)
-        {
-            LOG_COMM("Executing joint feedback unload");
-
-            if (!buffer->unload(this->group_number))
-            {
-                LOG_ERROR("Failed to unload hc feedback group_number");
-                return false;
-            }
-
-            if (!buffer->unload(this->jointTorques))
-            {
-                LOG_ERROR("Failed to unload hc feedback jointTorques");
-                return false;
-            }
-
-            if (!buffer->unload(this->tcpForces))
-            {
-                LOG_ERROR("Failed to unload hc feedback tcpForces");
-                return false;
-            }
-
-            LOG_COMM("Joint feedback successfully unloaded");
-            return true;
-        }
-
-    }  // namespace hc_feedback
-}  // namespace industrial
-
-
+//
+//#include <vector>
+//
+//#ifndef FLATHEADERS
+//#include "motoman_driver/simple_message/hc_feedback.h"
+//#include "simple_message/shared_types.h"
+//#include "simple_message/log_wrapper.h"
+//#else
+//#include "hc_feedback.h"  // NOLINT(build/include)
+//#include "shared_types.h"       // NOLINT(build/include)
+//#include "log_wrapper.h"        // NOLINT(build/include)
+//#endif
+//
+////using industrial::hc_feedback_message::HcFeedbackMessage;
+//using industrial::joint_feedback::JointFeedback;
+//
+//namespace industrial
+//{
+//    namespace hc_feedback
+//    {
+//
+//        HcFeedback::HcFeedback(void)
+//        {
+//            this->init();
+//        }
+//
+//        HcFeedback::~HcFeedback(void)
+//        {
+//        }
+//
+//        void HcFeedback::init()
+//        {
+//        }
+//
+//        void HcFeedback::copyFrom(HcFeedback& src)
+//        {
+//            group_number = src.getGroupNumber();
+//            jointTorques = src.getJointTorques();
+//            tcpForces = src.getTcpForces();
+//        }
+//
+//        bool HcFeedback::operator==(HcFeedback& rhs)
+//        {
+//            return (group_number == rhs.getGroupNumber() &&
+//                jointTorques == rhs.getJointTorques() &&
+//                tcpForces == rhs.getTcpForces());
+//        }
+//
+//        bool HcFeedback::load(industrial::byte_array::ByteArray* buffer)
+//        {
+//            LOG_COMM("Executing hc feedback load");
+//
+//            if (!buffer->load(this->group_number))
+//            {
+//                LOG_ERROR("Failed to load hc feedback groups_number");
+//                return false;
+//            }
+//
+//            if (!buffer->load(this->jointTorques))
+//            {
+//                LOG_ERROR("Failed to load jointTorques");
+//                return false;
+//            }
+//
+//            if (!buffer->load(this->tcpForces))
+//            {
+//                LOG_ERROR("Failed to load jointTorques");
+//                return false;
+//            }
+//
+//            LOG_COMM("hc feedback successfully loaded");
+//            return true;
+//        }
+//
+//        bool HcFeedback::unload(industrial::byte_array::ByteArray* buffer)
+//        {
+//            LOG_COMM("Executing joint feedback unload");
+//
+//            if (!buffer->unload(this->group_number))
+//            {
+//                LOG_ERROR("Failed to unload hc feedback group_number");
+//                return false;
+//            }
+//
+//            if (!buffer->unload(this->jointTorques))
+//            {
+//                LOG_ERROR("Failed to unload hc feedback jointTorques");
+//                return false;
+//            }
+//
+//            if (!buffer->unload(this->tcpForces))
+//            {
+//                LOG_ERROR("Failed to unload hc feedback tcpForces");
+//                return false;
+//            }
+//
+//            LOG_COMM("Joint feedback successfully unloaded");
+//            return true;
+//        }
+//
+//    }  // namespace hc_feedback
+//}  // namespace industrial
+//
+//
